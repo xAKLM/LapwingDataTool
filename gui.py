2e5faa2c8e5f48525ee6dedb5e80187bbf22a97e
Ben <benjaminvonsnarski@gmail.com>
2021-08-06 00:39:26 +1000

    Setup command function for implementation

--- gui.py
+++ gui.py
@@ -7,7 +7,6 @@ import tool as tl
 
 TITLE = "Lapwing Data Tool"
 RESOLUTION = "1280x720"
-vFRAME_relW = 0.8
 
 
 class App(tk.Tk):
@@ -19,21 +18,41 @@ class App(tk.Tk):
         # Initialise components
         self._mFrame = tk.Frame(self)
         self._vFrame = tk.Frame(self)
-        self._tool = tl.Tool("arty-primary.csv")
+        self._entry = tk.Entry(self._vFrame)
+        self._entry.bind("<Return>", lambda event: self.enter(self._entry.get()))
         self._text = tk.Text(self._vFrame)
 
-        self._mFrame.place(anchor=tk.SE, relx=1, rely=1, relheight=1, relwidth=1 - vFRAME_relW)
-        self._vFrame.place(anchor=tk.NW, relx=0, rely=0, relheight=1, relwidth=vFRAME_relW)
-        self._text.pack(expand=True, fill=tk.BOTH)
+        self.set_components()
 
+        self._tool = tl.Tool("arty-primary.csv")
         self._btn = tk.Button(self._mFrame, text="TEST", command=self.temp)
         self._btn.pack()
+
         self.load_data()
+
+    def set_components(self):
+        self._mFrame.place(anchor=tk.SE, relx=1, rely=1, relheight=1, relwidth=0.2)
+        self._vFrame.place(anchor=tk.NW, relx=0, rely=0, relheight=1, relwidth=0.8)
+        self._entry.place(anchor=tk.SE, relx=1, rely=1, height=24, relwidth=1)
+        self._text.place(anchor=tk.NW, relx=0, rely=0, height=696, relwidth=1)
+
+    def enter(self, command):
+        self.clear_entry()
+
+        if len(command) == 0 or command[0] != '/':
+            return
+
+        line = command[1:]
+        if line != "test":
+            self.handle_error("test is the only implemented command")
+        else:
+            self.replace_text(line)
+            self.after(2000, self.temp)
 
     def temp(self):
         try:
             stf = self._tool.get_outliers("Altitude")
-            self.replace(stf)
+            self.replace_text(stf)
             print("yo")
         except:
             self.handle_error()
@@ -41,17 +60,20 @@ class App(tk.Tk):
     def load_data(self):
         try:
             dfstr = self._tool.get_data().to_string()
-            self.replace(dfstr)
+            self.replace_text(dfstr)
         except:
             self.handle_error()
 
     def init_vFrame(self):
         pass
 
-    def replace(self, txt):
+    def replace_text(self, text):
         self._text.delete(1.0, "end")
-        self._text.insert(1.0, txt)
+        self._text.insert(1.0, text)
+
+    def clear_entry(self):
+        self._entry.delete(0, "end")
 
     @staticmethod
-    def handle_error():
-        alert.showerror("Error", sys.exc_info())
+    def handle_error(msg=None):
+        alert.showerror("Error", msg if msg is not None else sys.exc_info())
